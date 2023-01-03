1d4aee715bd0fafc190a882241ae45867cdbd0d7
lifiij <migoviprog@gmail.com>
2023-01-03 15:35:18 +0400

    test for gh-pages

---
+++ src/components/main.vue
@@ -0,0 +1,10 @@
+<template>
+  <main></main>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class MainComponent extends Vue {}
+</script>
--- src/views/PageLikes.vue
+++ src/views/PageLikes.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     PageLikes
-    <card />
     <miniCard />
   </div>
 </template>
