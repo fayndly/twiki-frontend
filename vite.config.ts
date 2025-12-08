2a8c2145b41127f743efd0152a2950bdafb43a92
fayndly <fayndly@gmail.com>
2025-12-08 16:22:51 +0400

    organized FSD structure

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/styles/index" as *;`
+        additionalData: `@use "@/app/styles/index" as *;`
       }
     }
   }
