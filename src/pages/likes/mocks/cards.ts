b3e30f8b6e9490412249e817b32f257074fd4b4a
fayndly <fayndly@gmail.com>
2025-12-13 15:33:56 +0400

    updated card profile animation swipe and connected functionality viewing cards to likes page

--- src/pages/likes/mocks/cards.ts
+++ src/pages/likes/mocks/cards.ts
@@ -2,6 +2,9 @@ import { type ICardProfile } from "../types/index.types";
 
 export const cards: ICardProfile[] = [
   {
+    isLiked: false,
+    isDisliked: false,
+    id: 5,
     userId: "fayndly",
     imgUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX479vqWzMi9zRT-spJ2DYqM2MBfweIHgPAA&s",
@@ -9,10 +12,12 @@ export const cards: ICardProfile[] = [
     age: 25,
     city: "Саранск",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus distinctio quod odit temporibus earum illo, expedita delectus in, praesentium enim velit quibusdam? Aliquam assumenda sapiente expedita amet perspiciatis similique rem?",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus distinctio quod odit temporibus earum illo, expedita delectus in, praesentium enim velit quibusdam? Aliquam assumenda sapiente expedita amet perspiciatis similique rem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus distinctio quod odit temporibus earum illo, expedita delectus in, praesentium enim velit quibusdam? Aliquam assumenda sapiente expedita amet perspiciatis similique rem?",
   },
-
   {
+    isLiked: false,
+    isDisliked: false,
+    id: 4,
     userId: "fayndly",
     imgUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSns3P0ddhqag0tCUb-UqdOMPpnDeIoCLHkQg&s",
@@ -21,8 +26,10 @@ export const cards: ICardProfile[] = [
     city: "District of Columbia",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
-
   {
+    isLiked: false,
+    isDisliked: false,
+    id: 3,
     userId: "fayndly",
     imgUrl:
       "https://i.pinimg.com/474x/7e/f9/37/7ef9378755b9e25c20b86b968c1bcbb3.jpg",
@@ -32,8 +39,10 @@ export const cards: ICardProfile[] = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus distinctio quod odit temporibus earum illo.",
   },
-
   {
+    isLiked: false,
+    isDisliked: false,
+    id: 2,
     userId: "fayndly",
     imgUrl:
       "https://images.thevoicemag.ru/upload/img_cache/e70/e707eb96cc68b5a7219e34fe6446f645_cropped_666x833.jpg",
@@ -43,8 +52,10 @@ export const cards: ICardProfile[] = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus distinctio quod odit temporibus earum illo, expedita delectus in, praesentium enim velit quibusdam?",
   },
-
   {
+    isLiked: false,
+    isDisliked: false,
+    id: 1,
     userId: "fayndly",
     imgUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGyJjq3op08sK2Wc-_od5Xckv9mfQN_LKejA&s",
--- src/pages/likes/types/index.types.ts
+++ src/pages/likes/types/index.types.ts
@@ -2,4 +2,5 @@ import { type IPropsCardProfile } from "@/widgets/CardProfile";
 
 export interface ICardProfile extends IPropsCardProfile {
   userId: string;
+  id: number;
 }
