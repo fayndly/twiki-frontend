6896c671026802b95bc2aeb90e3d6a25cb7b7276
fayndly <fayndly@gmail.com>
2026-03-09 17:09:19 +0400

    fixed clors in inputs

--- src/shared/inputs/SubtitleInput/index.ts
+++
@@ -1,2 +0,0 @@
-export { SubtitleInput } from "./ui/SubtitleInput.tsx";
-export { type IPropsInputSubtitle } from "./types/index.types.ts";
--- src/shared/inputs/SubtitleInput/types/index.types.ts
+++
@@ -1,4 +0,0 @@
-export interface IPropsInputSubtitle {
-  errors?: string | undefined;
-  subtitle?: string;
-}
